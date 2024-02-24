fffc7965f5d880c6a0fe963dda25d5b647ef4ecf
HuizhongWang <minasopor23@gmail.com>
2024-02-24 19:23:13 +1300

    Update Biosecurity.sql

--- Biosecurity.sql
+++ Biosecurity.sql
@@ -3,12 +3,13 @@ use Biosecurity;
 CREATE TABLE IF NOT EXISTS forester
 (
 forester_id INT auto_increment PRIMARY KEY NOT NULL,
-first_name varchar(30),
+first_name varchar(30) not null,
 family_name varchar(30) not null,
 status_now varchar(8) not null,
 address varchar(500) not null,
 email varchar(300) not null,
-phone INT not null
+phone varchar(11) not null,
+date_joined date not null
 );
 
 
@@ -17,8 +18,9 @@ CREATE TABLE IF NOT EXISTS staff
 staff_id INT auto_increment PRIMARY KEY NOT NULL,
 first_name varchar(30),
 family_name varchar(30) not null,
+status_now varchar(8) not null,
 email varchar(300) not null,
-phone INT not null,
+phone varchar(11) not null,
 hire_date date not null,
 staff_position varchar(100) not null,
 department varchar(100) not null
@@ -29,31 +31,33 @@ CREATE TABLE IF NOT EXISTS admini
 admin_id INT auto_increment PRIMARY KEY NOT NULL,
 first_name varchar(30),
 family_name varchar(30) not null,
+status_now varchar(8) not null,
 email varchar(300) not null,
-phone INT not null,
+phone varchar(11) not null,
 hire_date date not null,
 admin_position varchar(100) not null,
 department varchar(100) not null
 );
 
-CREATE TABLE IF NOT EXISTS forestry_guide
+CREATE TABLE IF NOT EXISTS forestry
 (
 forestry_id INT auto_increment PRIMARY KEY NOT NULL,
 forestry_type  varchar(7) not null ,
 present_in_nz varchar(3) not null,
 common_name varchar(320) not null,
-primary_image varchar(500) not null
+scientific_name varchar(100) not null ,
+key_characteristics text,
+biology text,
+symptoms text,
+image_num int
 );
 
-CREATE TABLE IF NOT EXISTS forestry_detail
+CREATE TABLE IF NOT EXISTS images
 (
+image_num INT auto_increment PRIMARY KEY NOT NULL,
 forestry_id INT NOT NULL,
-scientific_name varchar(100) not null ,
-key_characteristics text not null,
-biology text not null,
-symptoms text not null,
-images varchar(500),
-FOREIGN KEY (forestry_id) REFERENCES forestry_guide(forestry_id)
-ON UPDATE CASCADE
+images mediumblob,
+FOREIGN KEY (forestry_id) REFERENCES forestry(forestry_id)
+on update cascade
 );
 
