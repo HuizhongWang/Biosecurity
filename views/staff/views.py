15dbd8d8a6282a01a130cd751fbf87f549aeb0cf
HuizhongWang <minasopor23@gmail.com>
2024-03-13 14:33:15 +1300

    modify image route
    pythonanywhere upload route must not be relative path

--- views/staff/views.py
+++ views/staff/views.py
@@ -73,12 +73,11 @@ def s_index():
                 if "fileimg" in request.files:
                     file_img = request.files.get("fileimg")
                     file_name = file_img.filename
-                    file_path = file_name
+                    file_path = "/Users/doubleluo/Documents/GitHub/Biosecurity/static/imgdata/" + file_name
                     file_img.save(file_path)
-                    file_name = "imgdata/"+file_name
                     connection.execute("select images from images where forestry_id=%s",(forid,))
                     images = connection.fetchall() 
-                    if images:   
+                    if images:   # the forestry already have images
                         connection.execute("insert into images (forestry_id,images) values (%s,%s)", (forid,file_name,))
                     else:
                         connection.execute("update images set images=%s where forestry_id=%s", (file_name,forid,))
@@ -129,10 +128,8 @@ def s_guide():
                 if "fileimg" in request.files:
                     file_img = request.files.get("fileimg")
                     file_name = file_img.filename
-                    file_path = "BIOSECURITY/static/imgdata/"+file_name
+                    file_path = "/Users/doubleluo/Documents/GitHub/Biosecurity/static/imgdata/" + file_name
                     file_img.save(file_path)
-                    file_name = "imgdata/"+file_name
-                    print(file_name)
                     connection.execute("insert into images (forestry_id,images,show_p) values (%s,%s,1)", (forid,file_name,))
                 else:
                     flash("Fail to save the image","danger")
